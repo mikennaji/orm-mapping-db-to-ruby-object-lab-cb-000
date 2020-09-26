2bc66fccd093a009716bec7886ab9149cc90ae69
Michael Nnaji <mnnaji3@uwo.ca>
2020-09-26 17:54:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
   def self.students_below_12th_grade
     sql = <<-SQL
     SELECT * FROM students
-    WHERE grade < 12;
+    WHERE grade <12;
     SQL
     DB[:conn].execute(sql)
       
