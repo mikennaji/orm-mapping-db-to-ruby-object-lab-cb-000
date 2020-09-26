e16f4a261a89a82ec3b7d94c65b5291d8919af9c
Michael Nnaji <mnnaji3@uwo.ca>
2020-09-26 17:48:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,6 +21,14 @@ class Student
     # remember each row should be a new instance of the Student class
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students 
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+    
+  end
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM students
